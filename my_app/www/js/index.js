0b9708cab9bc57d2dae2f1d4550bb0785cf9de51
philippe-jonathan <philippe1jonathan@gmail.com>
2018-12-21 17:01:18 +0100

    save value + show value

--- my_app/www/js/index.js
+++ my_app/www/js/index.js
@@ -1,27 +1,29 @@
-
 //création de la base de données.
 //creation of the database.
 function create_db(tx) {
     tx.executeSql('CREATE TABLE task ( `id` int(11) NOT NULL, `name` text NOT NULL, `date` date NOT NULL, `time` time NOT NULL)'); 
 }
+
 //Configuration de la base de données.
 //Configuration of the database.
 var db = window.openDatabase('databse_cordova', '1.0', 'bdd_cordova', 1000000);
 db.transaction(create_db);
 
-//ajout des valeurs dans la base de données.
-//adding values ​​to the database.
+//fonction qui sert à ajouter des valeurs dans la base de données.
 function insert(tx){
     tx.executeSql('INSERT INTO task (id, name, date, time) VALUES (' + 1 + ', "' + name + '", "'+ date + '", "'+ time + '")');
 }
 
-//initialisation des variable correspondantes au input.
+//On affiche toutes les données entrées précédemment dans la base de données.
+db.transaction(dispAll);
+
+//initialisation des variable correspondantes au input plus l'id = 0.
 var name;
 var date;
 var time;
+var id = 0;
 
-//au click du boutton on envoie les valeurs des inputs dans la base de données et on les récupères.
-//at the click of the button the values of the inputs are sent to the database and displayed.
+//au click du boutton on récupère les valeurs des input et on séléctionne toutes les valeurs de la table.
 $('#submit_btn').click(function(){
     name = $('#name').val();
     date = $('#date').val();
@@ -33,31 +35,36 @@ $('#submit_btn').click(function(){
 //fonction qui sert a afficher.
 function dispAll(tx)
 {
+    //on récupère toutes les valeurs de la base de données.
     tx.executeSql('SELECT * FROM task;', [], function(tx, result){
         task = result.rows;
         console.log(task);
-        for (i=0; i<task.length; i++) {
+        for (i = 0; i<task.length; i++) {
             
-            $(".result").append("<tr>"+
-            "<th scope='col'>" + 1 + "</th>"+
-            "<td scope='col'>" + task[i].name + "</td>"+
-            "<td scope='col'>" + task[i].date + "</td>"+
-            "<td scope='col'>" + task[i].time + "</td></tr>")
-            
+            //et on affichage le tout dans la div "class=result".
+            $(".result").append(
+                "<div id='tasks_infos' class='text-center'>"
+                    + task[i].id + " " 
+                    + task[i].name + " "
+                    + task[i].date + " "
+                    + task[i].time 
+                + "</div>")
         }
     })
-
 };
 
-//On affiche toutes les données de la base de données.
-db.transaction(dispAll);
 
-//fonction qui sert à selctionner toutes les valeurs de la base de données.
+//fonction qui sert à séléctionner toutes les valeurs de la base de données.
 function selectAll(tx)
 {
+    //on récupère toutes les valeurs de la base de données.
     tx.executeSql('SELECT * FROM task;', [], function(tx, result){
         task = result.rows;
+        for( i = 0; i<task.length;i++){
+            id = i;
+        }
         console.log(task);
+        //on utilise la fonction insert en même temps que la fonction selectAll pour envoyer les données dans la table.
         db.transaction(insert);
       })
 
